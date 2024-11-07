91117fd6b50f1889fe545e0a7978abd11d8f0d70
DanielF-Cardoso <danielfca122@gmail.com>
2024-11-07 13:48:01 -0300

    chore: install and configure bcryptjs for user password encryption

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 
 export async function httpRoutes(app: FastifyInstance) {
-    app.post('/users', register)
+  app.post('/users', register)
 }
